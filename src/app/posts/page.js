39a999fd13d07f1e6aebb1ef486b8939089d6781
“hxc338” <hxc338@student.bham.ac.uk>
2025-02-07 11:57:39 +0000

    finished basic structure of posts page

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,15 +1,27 @@
+import { db } from "../utils/dbConnection"
 import Link from "next/link" // give your user some navigation controls!
 // remember to add metadata for the page 
 // We need to be able to sort the posts by asc and desc order. Query strings!
 
-export default function PostsPage(){
+export default async function PostsPage(){
+    const posts = await db.query(`SELECT * FROM posts`)
+    console.log(posts)
+
+    const wrangledPosts = await posts.rows
+    console.log(wrangledPosts)
     return (
         <>
             <Link href={"/"}>Home</Link>
             <h1>Posts Page</h1>
+
             {/* I need to get all posts from my database */}
             {/* I need to render all of the posts */}
-            Sort the 
+
+            {wrangledPosts.map((item) => (
+                <div key={item.id}>
+                    <h2>{item.post_title}</h2>
+                </div>
+            ))}
         </>
     )
 }
