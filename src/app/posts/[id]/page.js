98972f49ca77aa490aa6d58efbedf8a7d880b41a
“hxc338” <hxc338@student.bham.ac.uk>
2025-02-07 13:50:43 +0000

    A post can be displayed and the comments for it

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,19 +1,56 @@
+import { db } from "@/app/utils/dbConnection"
+
 // Give your user some naviagtion controls
 // remember to add metadata for the page 
 // We need params to render the post data dunamically!
 
-export default function IdPage ({params}){
+export default async function IdPage ({params}){
     // I need to get posts from supabase but filtered by the id 
     // (WHERE id = ${1/params })
     //  I need to handle the submit of the comments form 
 
+    const postParams = await params;
+    console.log(postParams)
+
+    const post = await db.query(`SELECT * FROM posts WHERE id = $1`, [postParams.id])
+    console.log(post)
+
+    const wrangledPost = post.rows
+    console.log(wrangledPost)
+
+    const postComment = await db.query(
+        `SELECT comments.comment, comments.author FROM posts
+        JOIN comments ON comments.post_id = posts.id WHERE posts.id = $1`, [postParams.id])
+    console.log(postComment)
+    const wrangledPostComment = postComment.rows
+    console.log(wrangledPostComment)
+
+
+
     return (
         <>
-            <h1> This is a post: </h1>
             <h1> Dynamic route for individual post </h1>
+
+            <h2> Param: {postParams.id} </h2>
+
+            {wrangledPost.map((item)=>(
+                <div key={item.id}>
+                    <h3>Post Title: {item.post_title}</h3>
+                </div>
+            ))}
+            <br/>
+            {wrangledPostComment.map((item)=> (
+                <div key={item.id}>
+                    <h4>{item.comment}</h4>
+                    <h5>By: {item.author}</h5>
+                    <br/>
+                </div>
+
+            ))}
+
             {/* Display the individual form */}
             {/* Display any comments on the post */}
-            {/* Need to display teh comments form to capture the users comments  */}
+            {/* Need to display the comments form to capture the users comments  */}
         </>
     )
 }
