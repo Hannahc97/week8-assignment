4bb285a4b02875cbe6ebd496e93e292223bd9975
“hxc338” <hxc338@student.bham.ac.uk>
2025-02-08 00:09:49 +0000

    made delete button but might need move server action outside jsx

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,5 +1,7 @@
 import { db } from "../utils/dbConnection"
 import Link from "next/link" // give your user some navigation controls!
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation"
 // remember to add metadata for the page 
 // We need to be able to sort the posts by asc and desc order. Query strings!
 
@@ -21,6 +23,21 @@ export default async function PostsPage({searchParams}){
         })
     }
 
+    // async function handleDelete(postId){
+    //     "use server"
+
+    //     await db.query(`DELETE FROM posts WHERE id = $1`, [postId])
+    //     revalidatePath("/posts")
+    //     redirect("/posts")
+    // }
+
+    async function handleDeletePost (postId) {
+        "use server"
+        await db.query(`DELETE FROM posts WHERE id = $1`, [postId])
+        revalidatePath("/posts")
+        redirect("/posts")
+    }
+
 
     return (
         <>
@@ -37,6 +54,16 @@ export default async function PostsPage({searchParams}){
             {wrangledPosts.map((item) => (
                 <div key={item.id}>
                     <h2>{item.post_title}</h2>
+                    <button type="submit"
+                            // onClick={() => handleDelete(item.id)}
+                            onClick={async function deletePost (){
+                                "use server"
+                                await db.query(`DELETE FROM posts WHERE id = $1`, [item.id])
+                                revalidatePath("/posts")
+                                redirect("/posts")
+                            }}
+                        >Delete Post</button>
+                    <br/>
                 </div>
             ))}
         </>
